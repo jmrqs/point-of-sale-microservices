8ee08148b8a04de1b025e61e737a5c917345f6f2
jmrqs <74171400+jmrqs@users.noreply.github.com>
2023-04-10 17:23:33 -0300

    initial commit

---
+++ point-of-sale-products/PointOfSale.Products.Api/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Contact = new OpenApiContact
+        {
+            Name = "Point of Sale",
+            Url = new Uri("https://github.com/jmrqs/point-of-sale-overview")
+        }
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
